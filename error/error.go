3e596426104e7b80308f27acf1d2cfeb838a7206
CptPie <23438606+CptPie@users.noreply.github.com>
2023-04-11 19:26:54 +0200

    Added initial implementation of PL/B'esk errors
    These errors have a custom printing function that should be better
    readable than the sunbelt/default error prompts.
    
    The format is as follows:
    Error {error code}: {error message}
    Location: {filename} {line}:{column}
    {line contents}
    ^ (pointer to where the "wrong" token starts)

---
+++ error/error.go
@@ -0,0 +1,38 @@
+package error
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// PLBError is a custom error type for PLB errors
+type PLBError struct {
+	ErrorCode string // Error code, e.g. E501
+	Message   string // Error message, e.g. Invalid token type
+	File      string // Filename where the error occurred
+	Line      int    // Line number where the error occurred
+	Column    int    // Column number where the error occurred
+	LineText  string // Literal line contents where the error occurred
+}
+
+// NewPLBError creates a new PLBError
+func NewPLBError(errorCode string, message string, file string, line int, column int, lineText string) *PLBError {
+	return &PLBError{
+		ErrorCode: errorCode,
+		Message:   message,
+		File:      file,
+		Line:      line,
+		Column:    column,
+		LineText:  lineText,
+	}
+}
+
+// Error returns a string representation of the error
+func (e *PLBError) Error() string {
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("Error %s: %s\n", e.ErrorCode, e.Message))
+	buffer.WriteString(fmt.Sprintf("Location: %s %d:%d\n", e.File, e.Line, e.Column))
+	buffer.WriteString(fmt.Sprintf("%s\n", e.LineText))
+	buffer.WriteString(fmt.Sprintf("%s^\n", bytes.Repeat([]byte(" "), e.Column-1)))
+	return buffer.String()
+}
